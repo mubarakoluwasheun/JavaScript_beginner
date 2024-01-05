cb6b6a7297635141111578852878b5096be5b1fb
mubarakoluwasheun <mubarakoluwasheun@gmail.com>
2024-01-05 11:01:10 -0600

    chrome extension

--- Chrome-extension/index.js
+++ Chrome-extension/index.js
@@ -3,18 +3,20 @@ const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
 const ulEl = document.getElementById("ul-el")
 
-leadsFromLocalStorage = JSON.pass(localStorage.getItem("myLeads"))
-console.log(leadsFromLocalStorage)
+let leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"))
 
+if (leadsFromLocalStorage) {
+	myLeads = leadsFromLocalStorage
+	renderLeads()
+}
 
 inputBtn.addEventListener("click", function() {
 	myLeads.push(inputEl.value)
 	inputEl.value = ""
 	myLeads = localStorage.setItem("myLeads", JSON.stringify(myLeads))
 	renderLeads()
-
-
 })
+
 function renderLeads() {
 	let listItems = ""
 	for (let i = 0; i < myLeads.length; i++) {
