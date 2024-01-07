bc6127acf9d95130a644c5c51b5c089418ae35fc
mubarakoluwasheun <mubarakoluwasheun@gmail.com>
2024-01-07 04:43:19 -0600

    Javascript basics practices

--- LearningJSBasics/Practice Lesson/index.js
+++ LearningJSBasics/Practice Lesson/index.js
@@ -29,3 +29,60 @@ function logArr(arr) {
 logArr(myCourses)
 
 // lesson 3 Save to localStorage
+localStorage.setItem("myCredits", "100")
+let myCredits = localStorage.getItem("myCredits")
+console.log(myCredits)
+
+// addEventlistener and object array
+let data = [
+    {
+        player: "jane",
+        score: 52
+    },
+    {
+        player: "Mark",
+        score: 41
+    }
+]
+
+let janeBtn = document.getElementById("jane-btn")
+
+janeBtn.addEventListener("click", function() {
+    console.log(data[0].score)
+})
+
+// Generate sentence
+function generateSentence(desc, arr) {
+    let inputStr = `The ${arr.length} ${desc} are`
+    const lstStr = arr.length - 1
+    for(let i = 0; i < arr.length; i++) {
+        if (i === lstStr) {
+            inputStr += arr[i]
+        } else {
+            inputStr += arr[i] + ", "
+        } 
+    }
+    return inputStr
+}
+
+const sentence = generateSentence("highest moutains", ["Mount Everest", "K2"])
+console.log(sentence)
+
+// render images
+const imgs = [
+    "images/1.jpg",
+    "images/2.jpg",
+    "images/3.jpg"
+]
+
+const container = document.getElementById("container")
+
+function renderImages() {
+    let imgsDOM = ""
+    for (let i = 0; i < imgs.length; i++) {
+        imgsDOM += 'img alt="Employee in the company" class="team-img" src="${imgs[i]}">'
+    }
+    container.innerHTML = imgsDOM
+}
+
+renderImages()
